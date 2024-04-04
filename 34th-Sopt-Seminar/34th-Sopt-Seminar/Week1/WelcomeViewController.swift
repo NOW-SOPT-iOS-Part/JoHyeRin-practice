16748f5908cf8450035a8e203124d45036e284cd
JoHyerin <gpfls468@gmail.com>
2024-04-05 00:52:38 +0900

    [Feat] #2 - 폰트, 이미지 적용

--- 34th-Sopt-Seminar/34th-Sopt-Seminar/Week1/WelcomeViewController.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Week1/WelcomeViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class WelcomeViewController: UIViewController {
     var id: String?
     
+    private let welcomeImage: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
+        imageView.image = UIImage(named: "welcomeImage")
+        
+        return imageView
+    }()
+    
     private let welcomeLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
         label.textColor = .black
         label.textAlignment = .center
         label.numberOfLines = 2
+        label.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
         
         return label
     }()
@@ -25,6 +33,7 @@ class WelcomeViewController: UIViewController {
         button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
         button.setTitle("메인으로", for: .normal)
         button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
         
         return button
     }()
@@ -34,6 +43,7 @@ class WelcomeViewController: UIViewController {
         button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
         button.setTitle("다시 로그인", for: .normal)
         button.setTitleColor(UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
         button.addTarget(self, action: #selector(backToLoginButtonTapped), for: .touchUpInside)
         
         return button
@@ -43,7 +53,7 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
-        [welcomeLabel, goMainButton, backToLoginButton].forEach {
+        [welcomeImage, welcomeLabel, goMainButton, backToLoginButton].forEach {
             self.view.addSubview($0)
         }
         
