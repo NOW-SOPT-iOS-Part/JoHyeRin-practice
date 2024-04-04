bcc0608b64f8c95d806cda4675d18229f0f98f2d
JoHyerin <gpfls468@gmail.com>
2024-04-05 00:18:28 +0900

    [Setting] - 프로젝트 초기 설정

---
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 조혜린 on 3/30/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        self.window = UIWindow(windowScene: windowScene)
+        
+        let viewController = LoginViewController()
+
+        let navigationController = UINavigationController(rootViewController: viewController)
+        self.window?.rootViewController = navigationController
+
+        self.window?.makeKeyAndVisible()
+    }
+}
